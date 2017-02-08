895cb154f604c1d87d58a4f2c554f308d092db73
Herve Pages <hpages@fhcrc.org>
2017-02-08 21:00:12 +0000

    - writeHDF5Array() can now be called without specifying 'file' and/or 'name'.   In that case getHDF5DumpFile() and/or getHDF5DumpName() are used,   respectively. - The HDF5Array object returned by writeHDF5Array() is no more invisible. - Fix bug in coercion to HDF5Array (was returning an object of class   DelayedArray when the object to coerce had dimnames on it). - Remove coercion from ANY to HDF5ArraySeed.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DelayedArray@126608 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/realize.R
+++ R/realize.R
@@ -7,6 +7,15 @@
 setGeneric("realize", function(x) standardGeneric("realize"))
 
 setMethod("realize", "ANY",
-    function(x) as(DelayedArray(x), getRealizeBackend())
+    function(x)
+    {
+        ans <- as(DelayedArray(x), getRealizeBackend())
+        ## Temporarily needed because coercion to HDF5Array currently drops
+        ## the dimnames. See R/writeHDF5Array.R in the HDF5Array package for
+        ## more information about this.
+        ## TODO: Remove line below when this is addressed.
+        dimnames(ans) <- dimnames(x)
+        ans
+    }
 )
 
