8a0f86a5b0d8f622bf983615e11fd486ec62e85f
Hervé Pagès <hpages@fredhutch.org>
2018-03-29 08:11:04 -0700

    Skip some tests on Windows
    Skip the tests using the RleArray realization backend on Windows in order
    to avoid 'R CMD check' TIMEOUT on the Windows build machines.

--- R/zzz.R
+++ R/zzz.R
@@ -5,10 +5,14 @@
 
 .test <- function()
 {
+    ## Skip this on Windows to avoid 'R CMD check' TIMEOUT on the Windows
+    ## build machines.
+    if (.Platform$OS.type != "windows") {
     cat("------------------------------------------------------------------\n")
     cat("Running tests with realization backend set to \"RleArray\" ...\n")
     setRealizationBackend("RleArray")
     BiocGenerics:::testPackage("DelayedArray")
+    }
 
     cat("\n")
     cat("------------------------------------------------------------------\n")
