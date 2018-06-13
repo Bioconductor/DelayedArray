b7766b0bc34070f514cb9ddb1077047557762445
Hervé Pagès <hpages@fredhutch.org>
2018-06-13 08:48:04 -0700

    Don't run tests using the HDF5Array realization backend anymore
    Those tests are now run as part of the HDF5Array package test suite.

--- R/zzz.R
+++ R/zzz.R
@@ -6,23 +6,11 @@
 
 .test <- function()
 {
-  ## Skip this on Windows to avoid 'R CMD check' TIMEOUT on the Windows
-  ## build machines.
-  if (.Platform$OS.type != "windows") {
-    cat("------------------------------------------------------------------\n")
-    cat("Running tests with realization backend set to \"RleArray\" ...\n")
-    setRealizationBackend("RleArray")
-    BiocGenerics:::testPackage("DelayedArray")
-  }
-
-  ## Skip this on 32-bit Windows to avoid 'R CMD check' TIMEOUT on the
-  ## Windows build machines.
-  if (.Platform$OS.type != "windows" || .Platform$r_arch != "i386") {
-    cat("\n")
-    cat("------------------------------------------------------------------\n")
-    cat("Running tests with realization backend set to \"HDF5Array\" ...\n")
-    setRealizationBackend("HDF5Array")
-    BiocGenerics:::testPackage("DelayedArray")
-  }
+    ## Skip this on Windows to avoid 'R CMD check' TIMEOUT on the Windows
+    ## build machines.
+    if (.Platform$OS.type != "windows") {
+        setRealizationBackend("RleArray")
+        BiocGenerics:::testPackage("DelayedArray")
+    }
 }
 
