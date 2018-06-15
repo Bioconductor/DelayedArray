cf623ef6ad24c17659fb82579992515e12e7be92
Hervé Pagès <hpages@fredhutch.org>
2018-06-15 10:02:55 -0700

    Add 'linear' argument to mapToGrid()

--- R/ArrayGrid-class.R
+++ R/ArrayGrid-class.R
@@ -530,12 +530,23 @@ setMethod("show", "ArrayGrid",
 ### 'aind' must be a numeric vector or matrix (a vector is treated
 ### like a 1-row matrix).
 setGeneric("mapToGrid", signature="grid",
-    function(aind, grid) standardGeneric("mapToGrid")
+    function(aind, grid, linear=FALSE) standardGeneric("mapToGrid")
 )
 
+.major_minor_as_list <- function(major, minor, grid, linear=FALSE)
+{
+    if (linear) {
+        major <- linearInd(major, dim(grid))
+        minor <- linearInd(minor, dims(grid)[major, , drop=FALSE])
+    }
+    list(major=major, minor=minor)
+}
+
 setMethod("mapToGrid", "ArbitraryArrayGrid",
-    function(aind, grid)
+    function(aind, grid, linear=FALSE)
     {
+        if (!isTRUEorFALSE(linear))
+            stop("'linear' must be TRUE or FALSE")
         ndim <- length(grid@tickmarks)
         aind <- normarg_aind(aind, ndim)
         major <- lapply(seq_len(ndim),
@@ -553,20 +564,24 @@ setMethod("mapToGrid", "ArbitraryArrayGrid",
                 aind[ , along] - offset[major[[along]]]
             }
         )
-        list(major=do.call(cbind, major), minor=do.call(cbind, minor))
+        major <- do.call(cbind, major)
+        minor <- do.call(cbind, minor)
+        .major_minor_as_list(major, minor, grid, linear=linear)
     }
 )
 
 setMethod("mapToGrid", "RegularArrayGrid",
-    function(aind, grid)
+    function(aind, grid, linear=FALSE)
     {
+        if (!isTRUEorFALSE(linear))
+            stop("'linear' must be TRUE or FALSE")
         ndim <- length(grid@spacings)
         aind <- normarg_aind(aind, ndim)
         d <- rep(grid@spacings, each=nrow(aind))
         aind0 <- aind - 1L  # 0-based indices
         major <- 1L + aind0 %/% d
         minor <- 1L + aind0 %% d
-        list(major=major, minor=minor)
+        .major_minor_as_list(major, minor, grid, linear=linear)
     }
 )
 
