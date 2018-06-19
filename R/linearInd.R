f2dbeb13347515cdf9cfd236c75f6a994a09cd1a
Hervé Pagès <hpages@fredhutch.org>
2018-06-19 09:44:50 -0700

    Add mapToRef() for doing the reverse of mapToGrid()

--- R/linearInd.R
+++ R/linearInd.R
@@ -5,20 +5,30 @@
 ### Performs the reverse transformation of arrayInd().
 ###
 
+.normarg_dim <- function(dim)
+{
+    if (!is.integer(dim) || S4Vectors:::anyMissingOrOutside(dim, 0L))
+        stop(wmsg("'dim' must be a vector (or matrix) ",
+                  "of non-negative integers with no NAs"))
+    if (!is.matrix(dim))
+        dim <- matrix(dim, nrow=1L)
+    dim
+}
+
 ### Return an integer matrix with 1 column per dimension.
-normarg_aind <- function(aind, ndim)
+normarg_aind <- function(aind, ndim, what="'aind'")
 {
     if (!is.numeric(aind))
-        stop(wmsg("'aind' must be a numeric vector or matrix"))
+        stop(wmsg(what, " must be a numeric vector or matrix"))
     if (is.matrix(aind)) {
         if (ncol(aind) != ndim)
-            stop(wmsg("'aind' must have one column (or element ",
+            stop(wmsg(what, " must have one column (or element ",
                       "if a vector) per dimension"))
     } else {
         if (is.array(aind))
-            stop(wmsg("'aind' must be a numeric vector or matrix"))
+            stop(wmsg(what, " must be a numeric vector or matrix"))
         if (length(aind) != ndim)
-            stop(wmsg("'aind' must have one element (or column ",
+            stop(wmsg(what, " must have one element (or column ",
                       "if a matrix) per dimension"))
         aind <- matrix(aind, ncol=ndim)
     }
@@ -32,17 +42,11 @@ normarg_aind <- function(aind, ndim)
 ### Return an integer vector with one element per row in 'aind'.
 linearInd <- function(aind, dim)
 {
-    if (!is.integer(dim) || S4Vectors:::anyMissingOrOutside(dim, 0L))
-        stop(wmsg("'dim' must be a vector (or matrix) ",
-                  "of non-negative integers with no NAs"))
-    if (!is.matrix(dim))
-        dim <- matrix(dim, nrow=1L)
+    dim <- .normarg_dim(dim)
     ndim <- ncol(dim)
     aind <- normarg_aind(aind, ndim)
     if (nrow(dim) != 1L && nrow(dim) != nrow(aind))
-        stop(wmsg("when a matrix, 'dim' must have the same shape ",
-                  "as 'aind', that is, 1 row per row in 'aind' ",
-                  "and 1 column per dimension"))
+        stop(wmsg("when a matrix, 'dim' must have 1 row per row in 'aind'"))
     if (ndim == 0L) {
         ans <- integer(nrow(aind))
     } else {
@@ -53,6 +57,46 @@ linearInd <- function(aind, dim)
         }
     }
     names(ans) <- rownames(aind)
+    ans
+}
+
+### Return a numeric vector.
+normarg_ind <- function(ind, what="'ind'")
+{
+    if (!is.numeric(ind))
+        stop(wmsg(what, " must be a numeric vector"))
+    if (is.matrix(ind))
+        stop(wmsg(what, " cannot be a matrix"))
+    if (suppressWarnings(min(ind, na.rm=TRUE)) < 1)
+        stop(wmsg(what, " must contain positive indices"))
+    ind
+}
+
+### An improved version of arrayInd() that accepts a matrix with 1 row per
+### element in 'ind' for 'dim'.
+### NOT exported.
+arrayInd2 <- function(ind, dim)
+{
+    dim <- .normarg_dim(dim)
+    ndim <- ncol(dim)
+    ind <- normarg_ind(ind)
+    if (nrow(dim) != 1L && nrow(dim) != length(ind))
+        stop(wmsg("when a matrix, 'dim' must have 1 row per element in 'ind'"))
+    if (ndim == 0L) {
+        ans <- matrix(integer(0), nrow=length(ind))
+    } else {
+        ind0 <- ind - 1L
+        d <- dim[ , 1L]
+        ans <- matrix(1L + as.integer(ind0 %% d), nrow=length(ind), ncol=ndim)
+    }
+    rownames(ans) <- names(ind)
+    if (ndim >= 2L) {
+        for (along in 2:ndim) {
+            ind0 <- ind0 %/% d
+            d <- dim[ , along]
+            ans[ , along] <- 1L + as.integer(ind0 %% d)
+        }
+    }
     ans
 }
 
--- R/mapToGrid.R
+++ R/mapToGrid.R
@@ -1,5 +1,5 @@
 ### =========================================================================
-### mapToGrid()
+### Map reference array positions to grid positions and vice-versa
 ### -------------------------------------------------------------------------
 
 
@@ -7,6 +7,10 @@
 ### like a 1-row matrix).
 setGeneric("mapToGrid", signature="grid",
     function(aind, grid, linear=FALSE) standardGeneric("mapToGrid")
+)
+
+setGeneric("mapToRef", signature="grid",
+    function(major, minor, grid, linear=FALSE) standardGeneric("mapToRef")
 )
 
 .major_minor_as_list <- function(major, minor, grid, linear=FALSE)
@@ -27,7 +31,7 @@ setMethod("mapToGrid", "ArbitraryArrayGrid",
         aind <- normarg_aind(aind, ndim)
         major <- lapply(seq_len(ndim),
             function(along) {
-                findInterval(aind[ , along], grid@tickmarks[[along]] + 1L) + 1L
+                1L + findInterval(aind[ , along], grid@tickmarks[[along]] + 1L)
             }
         )
         minor <- lapply(seq_len(ndim),
@@ -35,7 +39,7 @@ setMethod("mapToGrid", "ArbitraryArrayGrid",
                 tm <- grid@tickmarks[[along]]
                 tm_len <- length(tm)
                 if (tm_len == 0L)
-                    return(integer(0))
+                    return(rep.int(NA_integer_, nrow(aind)))
                 offset <- c(0L, tm[-tm_len])
                 aind[ , along] - offset[major[[along]]]
             }
@@ -43,6 +47,45 @@ setMethod("mapToGrid", "ArbitraryArrayGrid",
         major <- do.call(cbind, major)
         minor <- do.call(cbind, minor)
         .major_minor_as_list(major, minor, grid, linear=linear)
+    }
+)
+
+.normargs_major_minor <- function(major, minor, grid, linear)
+{
+    if (!isTRUEorFALSE(linear))
+        stop("'linear' must be TRUE or FALSE")
+    if (linear) {
+        major <- normarg_ind(major, what="when 'linear=TRUE', 'major'")
+        minor <- normarg_ind(minor, what="when 'linear=TRUE', 'minor'")
+        if (length(major) != length(minor))
+            stop(wmsg("when 'linear=TRUE', 'major' and 'minor' ",
+                      "must have the same length"))
+        minor <- arrayInd2(minor, dims(grid)[major, , drop=FALSE])
+        major <- arrayInd2(major, dim(grid))
+    } else {
+        ndim <- length(refdim(grid))
+        major <- normarg_aind(major, ndim, what="'major'")
+        minor <- normarg_aind(minor, ndim, what="'minor'")
+        if (nrow(major) != nrow(minor))
+            stop(wmsg("'major' and 'minor' must have the same number of rows"))
+    }
+    list(major=major, minor=minor)
+}
+
+setMethod("mapToRef", "ArbitraryArrayGrid",
+    function(major, minor, grid, linear=FALSE)
+    {
+        majmin <- .normargs_major_minor(major, minor, grid, linear)
+        ans <- majmin$minor
+        for (along in seq_len(ncol(ans))) {
+            tm <- grid@tickmarks[[along]]
+            tm_len <- length(tm)
+            if (tm_len == 0L)
+                next
+            offset <- c(0L, tm[-tm_len])
+            ans[ , along] <- ans[ , along] + offset[majmin$major[ , along]]
+        }
+        ans
     }
 )
 
@@ -58,6 +101,15 @@ setMethod("mapToGrid", "RegularArrayGrid",
         major <- 1L + aind0 %/% d
         minor <- 1L + aind0 %% d
         .major_minor_as_list(major, minor, grid, linear=linear)
+    }
+)
+
+setMethod("mapToRef", "RegularArrayGrid",
+    function(major, minor, grid, linear=FALSE)
+    {
+        majmin <- .normargs_major_minor(major, minor, grid, linear)
+        d <- rep(grid@spacings, each=nrow(majmin$major))
+        (majmin$major - 1L) * d + majmin$minor
     }
 )
 
