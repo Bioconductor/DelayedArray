089a4b19f698dc88e83d567df31d3f1f923c2086
Hervé Pagès <hpages@fredhutch.org>
2018-07-31 14:19:40 -0700

    Use SerialParam() backend instead of SnowParam() by default on Windows

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,13 @@
 {
     options(DelayedArray.simplify=TRUE)
     options(DelayedArray.block.size=DEFAULT_BLOCK_SIZE)
+    ## By default bplapply() uses the SnowParam() backend on Windows, which
+    ## introduces **a lot** of overhead in the context of block processing.
+    ## See https://github.com/Bioconductor/BiocParallel/issues/78
+    ## So we disable parallel evaluation by default on this platform, by
+    ## setting the default backend to SerialParam().
+    if (.Platform$OS.type == "windows")
+        BiocParallel::register(SerialParam())
 }
 
 .onUnload <- function(libpath)
