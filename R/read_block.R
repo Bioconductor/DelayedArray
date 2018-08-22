d021c88223c4f81944a82a68f13bc500a0b07e27
Hervé Pagès <hpages@fredhutch.org>
2018-08-22 03:15:41 -0700

    Add read_sparse_block() and write_sparse_block() generics

--- R/read_block.R
+++ R/read_block.R
@@ -1,8 +1,78 @@
 ### =========================================================================
-### The read_block() and write_block() generics
+### Read/write blocks of array data
 ### -------------------------------------------------------------------------
 ###
 
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### dense2sparse() and sparse2dense()
+###
+
+### Return a data.frame with 3 columns ("i", "j", "data") and 1 row per
+### non-zero element in matrix-like object 'x'.
+dense2sparse <- function(x)
+{
+    if (length(dim(x)) != 2L)
+        stop(wmsg("'x' must be a matrix-like object"))
+    aind <- which(x != 0L, arr.ind=TRUE)
+    data.frame(i=aind[ , "row"], j=aind[ , "col"], data=x[aind],
+               stringsAsFactors=FALSE)
+}
+
+### NOT exported and currently unused.
+dense2sparse.dgCMatrix <- function(x)
+{
+    stopifnot(is(x, "dgCMatrix"))
+    data.frame(i=x@i + 1L, j=rep.int(seq_len(ncol(x)), diff(x@p)), data=x@x)
+}
+
+### 'sparse_data' must be a data.frame as returned by dense2sparse().
+### Return an ordinary matrix.
+sparse2dense <- function(dim, sparse_data)
+{
+    if (!(is.numeric(dim) && length(dim) == 2L))
+        stop(wmsg("'dim' must have length 2"))
+    ans <- array(0L, dim=dim)
+    ans[cbind(sparse_data$i, sparse_data$j)] <- sparse_data$data
+    ans
+}
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### read_sparse_block() and write_sparse_block()
+###
+### Only supports matrix-like objects for now.
+###
+
+### Must return a data.frame as returned by dense2sparse().
+setGeneric("read_sparse_block", signature="x",
+    function(x, viewport)
+    {
+        x_dim <- dim(x)
+        stopifnot(length(x_dim) == 2L,
+                  is(viewport, "ArrayViewport"),
+                  identical(refdim(viewport), x_dim))
+        standardGeneric("read_sparse_block")
+    }
+)
+
+### 'sparse_block' must be a data.frame as returned by dense2sparse().
+### Must return 'x' (possibly modified if it's an in-memory object).
+setGeneric("write_sparse_block", signature="x",
+    function(x, viewport, sparse_block)
+    {
+        x_dim <- dim(x)
+        stopifnot(length(x_dim) == 2L,
+                  is(viewport, "ArrayViewport"),
+                  identical(refdim(viewport), x_dim))
+        standardGeneric("write_sparse_block")
+    }
+)
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### read_block() and write_block()
+###
 
 ### Must return an ordinay array and propagate the dimnames.
 setGeneric("read_block", signature="x",
