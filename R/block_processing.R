28630a034f1be6ec5d44561f3daaa49b404e23ed
Hervé Pagès <hpages@fredhutch.org>
2018-09-05 09:49:15 -0700

    Control default 'BPPARAM' used by blockApply() via set/getDefaultBPPARAM()

--- R/block_processing.R
+++ R/block_processing.R
@@ -40,6 +40,40 @@ setDefaultGridMaker <- function(GRIDMAKER="blockGrid")
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### get/setDefaultBPPARAM()
+###
+
+getDefaultBPPARAM <- function()
+{
+    getOption("DelayedArray.BPPARAM")
+}
+
+### By default (i.e. when no argument is specified), we set the default
+### BPPARAM to SerialParam() on Windows and to MulticoreParam() on other
+### platforms.
+### The reason we use SerialParam() instead of SnowParam() on Windows is
+### that the latter introduces **a lot** of overhead in the context of block
+### processing. See https://github.com/Bioconductor/BiocParallel/issues/78
+### To the point that disabling parallel evaluation (by using SerialParam())
+### is still much faster than parallel evaluation with SnowParam().
+setDefaultBPPARAM <- function(BPPARAM=NULL)
+{
+    if (is.null(BPPARAM)) {
+        if (.Platform$OS.type == "windows") {
+            BPPARAM <- BiocParallel::SerialParam()
+        } else {
+            BPPARAM <- BiocParallel::MulticoreParam()
+        }
+    } else {
+        if (!is(BPPARAM, "BiocParallelParam"))
+            stop(wmsg("'BPPARAM' must be a BiocParallelParam object"))
+    }
+    options(DelayedArray.BPPARAM=BPPARAM)
+    BPPARAM
+}
+
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### Walking on the blocks
 ###
 ### 2 utility functions to process array-like objects by block.
@@ -87,7 +121,7 @@ setDefaultGridMaker <- function(GRIDMAKER="blockGrid")
 ### setting the nb of tasks to the nb of blocks (i.e. with
 ### BPPARAM=MulticoreParam(tasks=length(grid))). However, in practice, that
 ### seems to be slower than using tasks=0 (the default). Investigate this!
-blockApply <- function(x, FUN, ..., grid=NULL, BPPARAM=bpparam())
+blockApply <- function(x, FUN, ..., grid=NULL, BPPARAM=getDefaultBPPARAM())
 {
     FUN <- match.fun(FUN)
     grid <- .normarg_grid(grid, x)
--- R/zzz.R
+++ R/zzz.R
@@ -4,13 +4,7 @@
     setDefaultGridMaker()
     suppressMessages(setDefaultBlockSize())
     suppressMessages(setDefaultBlockShape())
-    ## By default bplapply() uses the SnowParam() backend on Windows, which
-    ## introduces **a lot** of overhead in the context of block processing.
-    ## See https://github.com/Bioconductor/BiocParallel/issues/78
-    ## So we disable parallel evaluation by default on this platform, by
-    ## setting the default backend to SerialParam().
-    if (.Platform$OS.type == "windows")
-        BiocParallel::register(SerialParam())
+    setDefaultBPPARAM()
 }
 
 .onUnload <- function(libpath)
