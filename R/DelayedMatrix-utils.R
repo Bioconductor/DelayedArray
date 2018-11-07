747260b725450b3772203c300642df7ca426283d
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-07 15:56:49 +0000

    Added dispatcher functions to choose the parallelization scheme.

--- R/DelayedMatrix-utils.R
+++ R/DelayedMatrix-utils.R
@@ -132,10 +132,11 @@ setMethod("tcrossprod", c("ANY", "DelayedMatrix"), function(x, y) x %*% t(y))
     out
 }
 
-.split_by_dimension <- function(ndim, nworkers, boundaries) 
+.split_by_dimension <- function(boundaries, nworkers) 
 # Splits a dimension of the matrix into 'nworkers' jobs,
 # rounded up to the nearest 'boundaries' where possible. 
 {
+    ndim <- tail(boundaries, 1) # last boundary is always the matrix end.
     ideal_split <- pmin(ndim, ndim/nworkers * seq_len(nworkers))
     rounded <- findInterval(ideal_split, boundaries, left.open=TRUE) + 1L
     by_bound <- rle(rounded)
@@ -179,6 +180,92 @@ setMethod("tcrossprod", c("ANY", "DelayedMatrix"), function(x, y) x %*% t(y))
         current <- current + l
     }
 
-    list(bounds=limits, costs=costs)    
+    list(split=limits, cost=costs)    
 }
 
+.evaluate_split_costs <- function(split, boundaries) 
+# Given a split, evaluate the costs with respect to read atomicity. 
+# To be used when 'split' is computed from a different matrix than 'boundaries'.
+{
+    upper <- findInterval(split, boundaries, left.open=TRUE) + 1L
+    lower <- findInterval(head(split, -1L), boundaries)
+    boundaries[upper] - c(0L, boundaries[lower])
+}
+
+.dispatch_mult <- function(x, y, nworkers, transposed.x=FALSE, transposed.y=FALSE) 
+# Decides how to split jobs across multiple workers for x %*% y.
+# Assessing based on the maximum cost for a given worker under each scheme,
+# with the aim being to minimize the maximum time to complete all jobs.
+{
+    atomic_x <- .define_atomic_boundaries(x)
+    nr_x <- as.double(nrow(x)) # use double to avoid overflow.
+    nc_x <- as.double(ncol(x))
+
+    atomic_y <- .define_atomic_boundaries(y)
+    nr_y <- as.double(nrow(y))
+    nc_y <- as.double(ncol(y))
+
+    if (transposed.x) {
+        atomic_x <- list(row=atomic_x$col, col=atomic_x$row) 
+        tmp <- nr_x
+        nr_x <- nc_x
+        nc_x <- tmp
+    }
+
+    if (transposed.y) {
+        atomic_y <- list(row=atomic_y$col, col=atomic_y$row) 
+        tmp <- nr_y
+        nr_y <- nc_y
+        nc_y <- tmp
+    }
+
+    # Splitting 'x' by row, and multiplying with all of 'y'.
+    x_by_row <- .split_by_dimension(atomic_x$row, nworkers)
+    cost_x_by_row <- max(x_by_row$cost * nc_x) + as.double(length(y))
+
+    # Splitting 'y' by column and multiplying with all of 'x'.
+    y_by_col <- .split_by_dimension(atomic_y$col, nworkers)
+    cost_y_by_col <- as.double(length(x)) + max(nr_y * y_by_col$cost)
+
+    # Splitting both of them by the common dimension.
+    common_x <- .split_by_dimension(atomic_x$col, nworkers)
+    cost_common_x <- max(nr_x * common_x$cost + .evaluate_split_costs(common_x$split, atomic_y$row) * nc_y) 
+    common_y <- .split_by_dimension(atomic_y$row, nworkers)
+    cost_common_y <- max(nr_x * .evaluate_split_costs(common_y$split, atomic_x$col) + common_y$cost * nc_y)
+
+    all_options <- c(x_by_row=cost_x_by_row, y_by_col=cost_y_by_col, common_x=cost_common_x, common_y=cost_common_y)
+    choice <- names(all_options)[which.min(all_options)]
+    splits <- switch(choice, x_by_row=x_by_row$split, y_by_col=y_by_col$split, common_x=common_x$split, common_y=common_y$split)
+    reported <- switch(choice, x_by_row="left", y_by_col="right", "both")
+    list(choice=reported, split=splits)
+}
+
+.dispatch_self <- function(x, nworkers, transposed=FALSE) 
+# Decides how to split jobs across multiple workers for crossprod(x).
+{
+    atomic <- .define_atomic_boundaries(x)
+    nr <- as.double(nrow(x)) # use double to avoid overflow.
+    nc <- as.double(ncol(x))
+
+    if (transposed) {
+        atomic <- list(row=atomic$col, col=atomic$row) 
+        tmp <- nr
+        nr <- nc
+        nc <- tmp
+    }
+
+    # Splitting 'x' by column, and taking the crossproduct with all of 'x'.
+    # This is the same the other way around, so we don't bother computing that.
+    single <- .split_by_dimension(atomic$col, nworkers)
+    cost_single <- max(nr * single$cost) + as.double(length(x))
+
+    # Splitting by the common dimension, i.e., along the rows of 'x'
+    # and taking the crossproduct of each split block.
+    both <- .split_by_dimension(atomic$row, nworkers)
+    cost_both <- max(both$cost * nc) * 2
+
+    all_options <- c(both=cost_both, single=cost_single)
+    choice <- names(all_options)[which.min(all.options)]
+    splits <- switch(choice, both=both$split, single=single$split)
+    list(choice=choice, split=splits)
+}
