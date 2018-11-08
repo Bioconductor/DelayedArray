028026e5347cce63e4daf2dc767f8e8f737f1740
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-08 14:54:58 +0000

    Switched to bpiterate due to ArrayGrid bug with bplapply. Added methods for self crossprods.

--- R/DelayedMatrix-utils.R
+++ R/DelayedMatrix-utils.R
@@ -246,10 +246,11 @@ setMethod("%*%", c("DelayedMatrix", "DelayedMatrix"), .BLOCK_matrix_mult)
     all_options <- c(x_by_row=cost_x_by_row, y_by_col=cost_y_by_col, common_x=cost_common_x, common_y=cost_common_y)
     choice <- names(all_options)[which.min(all_options)]
 
-    if (choice=="x_by_row") {
-        grid <- list(x=x_grids$row)
-    } else if (choice=="y_by_row") {
-        grid <- list(y=y_grids$col)
+    if (choice=="x_by_row" || choice=="y_by_col") {
+        # Both are returned though only one is used at any given time;
+        # see .super_BLOCK_mult() for why.
+        grid <- list(x=x_grids$row, y=y_grids$col) 
+
     } else if (choice=="common_x") {
         grid <- list(x=x_grids$col, 
             y=ArbitraryArrayGrid(list(
@@ -273,6 +274,7 @@ setMethod("%*%", c("DelayedMatrix", "DelayedMatrix"), .BLOCK_matrix_mult)
     if (transposed.y && !is.null(grid$y)) {
         grid$y <- t(grid$y)
     }
+    choice <- switch(choice, x_by_row="x", y_by_col="y", common_x="common", common_y="common")
     list(choice=choice, grid=grid)
 }
 
@@ -292,22 +294,23 @@ setMethod("%*%", c("DelayedMatrix", "DelayedMatrix"), .BLOCK_matrix_mult)
         nc <- tmp
     }
 
-    by_row <- cumsum(dims(grids$row)[,1])
-    by_row_cost <- .evaluate_split_costs(by_row, atomic_x$row)
-    by_col <- cumsum(dims(grids$col)[,2])
-    by_col_cost <- .evaluate_split_costs(by_col, atomic_x$col)
-
     # Splitting 'x' by column, and taking the crossproduct with all of 'x'.
     # This is the same the other way around, so we don't bother computing that.
+    by_col <- cumsum(dims(grids$col)[,2])
+    by_col_cost <- .evaluate_split_costs(by_col, atomic$col)
     cost_single <- max(nr * by_col_cost) + nr * sum(by_col_cost)
 
     # Splitting by the common dimension, i.e., along the rows of 'x'
     # and taking the crossproduct of each split block.
+    by_row <- cumsum(dims(grids$row)[,1])
+    by_row_cost <- .evaluate_split_costs(by_row, atomic$row)
     cost_both <- max(by_row_cost * nc) * 2
 
     all_options <- c(both=cost_both, single=cost_single)
     choice <- names(all_options)[which.min(all_options)]
     grid <- switch(choice, both=grids$row, single=grids$col)
+
+    # Restoring grid for the original 'x'.
     if (transposed) {
         grid <- t(grid)
     }
@@ -322,11 +325,48 @@ setMethod("%*%", c("DelayedMatrix", "DelayedMatrix"), .BLOCK_matrix_mult)
 ### This also requires care to respect the maximum block size.
 ###
 
-.super_BLOCK_mult <- function(x, y, MULT, transposed.x=FALSE, transposed.y=FALSE, BPPARAM=bpparam()) {
-    if (!is(x, "DelayedMatrix") && !is(y, "DelayedMatrix")) {
-        return(MULT(x, y))
-    }
+.single_grid_iterate <- function(grid) {
+    force(grid)
+    b <- 0L
 
+    # Do NOT put read_block in here, as this will waste time realizing from file in the single worker.
+    # TODO: subset in-memory matrices to avoid serializing them in their entirety to the workers.
+    function () {
+        if (b==length(grid)) return(NULL)
+        b <<- b + 1L
+        grid[[b]]
+    }
+}
+
+.left_mult <- function(viewport, x, y, MULT) {
+    block <- read_block(x, viewport) # this, and all other calls, had better yield a non-DA, otherwise MULT will recurse endlessly.
+    MULT(block, y)
+}
+
+.right_mult <- function(viewport, x, y, MULT) {
+    block <- read_block(y, viewport)
+    MULT(x, block)
+}
+
+.dual_grid_iterate <- function(grid.x, grid.y) {
+    force(grid.x)
+    force(grid.y)
+    b <- 0L
+
+    function () {
+        if (b==length(grid.x)) return(NULL)
+        b <<- b + 1L
+        list(x=grid.x[[b]], y=grid.y[[b]])
+    }
+}
+
+.both_mult <- function(viewports, x, y, MULT) {
+    block_x <- read_block(x, viewports$x)
+    block_y <- read_block(y, viewports$y)
+    MULT(block_x, block_y)
+}
+
+.super_BLOCK_mult <- function(x, y, MULT, transposed.x=FALSE, transposed.y=FALSE, BPPARAM=bpparam()) {
     scheme_out <- .dispatch_mult(x, y, bpnworkers(BPPARAM), transposed.x=transposed.x, transposed.y=transposed.y)
     chosen_scheme <- scheme_out$choice
     chosen_grids <- scheme_out$grid
@@ -335,38 +375,30 @@ setMethod("%*%", c("DelayedMatrix", "DelayedMatrix"), .BLOCK_matrix_mult)
     on.exit(register(old))
     register(SerialParam()) # avoid re-parallelizing in further calls to 'MULT'.
 
-    if (chosen_scheme=="x_by_row") {
-        # TODO: figure out best serialization scheme.
-        out <- bplapply(chosen_grids[[1]], FUN=function(viewport, x, y) {
-            block <- read_block(x, viewport) # this, and all other calls, had better yield a non-DA, otherwise MULT will recurse endlessly.
-            MULT(block, y)
-        }, x=x, y=y, BPPARAM=BPPARAM)
-        ans <- do.call(rbind, out)
+    if (chosen_scheme=="common") {
+        ans <- bpiterate(.dual_grid_iterate(chosen_grids$x, chosen_grids$y), FUN=.both_mult,
+            x=x, y=y, MULT=MULT, BPPARAM=BPPARAM, REDUCE=function(x, y) x + y)
 
-    } else if (chosen_scheme=="y_by_col") {
-        out <- bplapply(chosen_grids[[1]], FUN=function(viewport, x, y) {
-            block <- read_block(y, viewport)
-            MULT(x, block)
-        }, x=x, y=y, BPPARAM=BPPARAM)
-        ans <- do.call(cbind, out)
+    } else {
+        # Switch to iteration over the other argument if the chosen one is 
+        # single-block and non-DA (at which point you might as well iterate
+        # over the other argument anyway). This avoids infinite recursion 
+        # when 'x' or 'y' fail to get realized via read_block().
+        if (chosen_scheme=="x" && length(chosen_grids$x)==1L && !is(x, "DelayedMatrix")) {
+            chosen_scheme <- "y"            
+        } else if (chosen_scheme=="y" && length(chosen_grids$y)==1L && !is(y, "DelayedMatrix")) {
+            chosen_scheme <- "x"
+        }
 
-    } else if (chosen_scheme=="common_x") {
-        # TODO: switch to bpiterate to reduce products once they are created.
-        out <- bpmapply(viewport_x=chosen_grids[[1]], viewport_y=chosen_grids[[2]], FUN=function(viewport_x, viewport_y, x, y) {
-            block_x <- read_block(x, viewport_x)
-            block_y <- read_block(y, viewport_y)
-            MULT(block_x, block_y)
-        }, MoreArgs=list(x=x, y=y), BPPARAM=BPPARAM, SIMPLIFY=FALSE)
-        ans <- Reduce("+", out)
-
-    } else  {
-        out <- bpmapply(viewport_x=chosen_grids[[1]], viewport_y=chosen_grids[[2]], FUN=function(viewport_x, viewport_y, x, y) {
-            block_x <- read_block(x, viewport_x)
-            block_y <- read_block(y, viewport_y)
-            MULT(block_x, block_y)
-        }, MoreArgs=list(x=x, y=y), BPPARAM=BPPARAM, SIMPLIFY=FALSE)
-        ans <- Reduce("+", out)
-
+        if (chosen_scheme=="x") {
+            out <- bpiterate(.single_grid_iterate(chosen_grids$x), FUN=.left_mult, 
+                x=x, y=y, MULT=MULT, BPPARAM=BPPARAM)
+            ans <- do.call(rbind, out)
+        } else if (chosen_scheme=="y") {
+           out <- bpiterate(.single_grid_iterate(chosen_grids$y), FUN=.right_mult,
+                x=x, y=y, MULT=MULT, BPPARAM=BPPARAM)
+           ans <- do.call(cbind, out)
+        }
     }
 
     ans
@@ -389,3 +421,35 @@ setMethod("tcrossprod", c("DelayedMatrix", "ANY"), function(x, y) .super_BLOCK_m
 setMethod("tcrossprod", c("ANY", "DelayedMatrix"), function(x, y) .super_BLOCK_mult(x, y, MULT=tcrossprod, transposed.y=TRUE))
 
 setMethod("tcrossprod", c("DelayedMatrix", "DelayedMatrix"), function(x, y) .super_BLOCK_mult(x, y, MULT=tcrossprod, transposed.y=TRUE))
+
+.solo_mult <- function(viewport, x, MULT) {
+    block <- read_block(x, viewport)
+    MULT(block)
+}
+
+.super_BLOCK_self <- function(x, MULT, transposed=FALSE, BPPARAM=bpparam()) {
+    scheme_out <- .dispatch_self(x, bpnworkers(BPPARAM), transposed=transposed)
+    chosen_scheme <- scheme_out$choice
+    chosen_grids <- scheme_out$grid
+
+    old <- bpparam()
+    on.exit(register(old))
+    register(SerialParam()) # avoid re-parallelizing in further calls to 'MULT'.
+
+    if (chosen_scheme=="single") {
+        out <- bpiterate(.single_grid_iterate(chosen_grids), FUN=.left_mult, 
+            x=x, y=x, MULT=MULT, BPPARAM=SerialParam())
+        ans <- do.call(rbind, out)
+
+    } else if (chosen_scheme=="both") {
+        ans <- bpiterate(.single_grid_iterate(chosen_grids), FUN=.solo_mult,
+            x=x, MULT=MULT, BPPARAM=BPPARAM, REDUCE=function(x, y) x+y)
+
+    }
+
+    ans
+}
+
+setMethod("crossprod", c("DelayedMatrix", "missing"), function(x, y) .super_BLOCK_self(x, MULT=crossprod))
+
+setMethod("tcrossprod", c("DelayedMatrix", "missing"), function(x, y) .super_BLOCK_self(x, MULT=tcrossprod, transposed=TRUE))
