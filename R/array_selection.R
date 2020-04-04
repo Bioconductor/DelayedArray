e19065cf62958f885feebacaf7470ca117c9405b
Hervé Pagès <hpages@fredhutch.org>
2020-04-04 12:51:26 -0700

    small tweak

--- R/array_selection.R
+++ R/array_selection.R
@@ -10,6 +10,11 @@ Lindex2Mindex <- function(Lindex, dim, use.names=FALSE)
 {
     if (!isTRUEorFALSE(use.names))
         stop("'use.names' must be TRUE or FALSE")
+    ## 'dim' can be a matrix so it's important to use storage.mode()
+    ## instead of as.integer().
+    if (storage.mode(dim) == "double")
+        storage.mode(dim) <- "integer"
+    ## 'Lindex' and 'dim' will be fully checked at the C level.
     .Call2("C_Lindex2Mindex", Lindex, dim, use.names, PACKAGE="DelayedArray")
 }
 
@@ -19,8 +24,14 @@ Mindex2Lindex <- function(Mindex, dim, use.names=FALSE, as.integer=FALSE)
         stop("'use.names' must be TRUE or FALSE")
     if (!isTRUEorFALSE(as.integer))
         stop("'as.integer' must be TRUE or FALSE")
-    if (storage.mode(Mindex) != "integer")
+    ## 'Mindex' and/or 'dim' can be matrices so it's important to use
+    ## storage.mode() instead of as.integer(). Also, unlike as.integer(),
+    ## this preserves the names/dimnames.
+    if (storage.mode(Mindex) == "double")
         storage.mode(Mindex) <- "integer"
+    if (storage.mode(dim) == "double")
+        storage.mode(dim) <- "integer"
+    ## 'Mindex' and 'dim' will be fully checked at the C level.
     .Call2("C_Mindex2Lindex", Mindex, dim, use.names,
                               as.integer, PACKAGE="DelayedArray")
 }
