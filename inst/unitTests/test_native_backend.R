98179173984b0f1944e0fd791d58f0764c8940ec
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-06-06 19:46:03 -0700

    Added tests for preservation of a native backend.

---
+++ inst/unitTests/test_native_backend.R
@@ -0,0 +1,95 @@
+
+test_native_cbind <- function() {
+    X <- Matrix::rsparsematrix(1000, 100, 0.1)
+    Dx <- DelayedArray(X)
+    Y <- Matrix::rsparsematrix(1000, 50, 0.1)
+    Dy <- DelayedArray(Y)
+
+    Dr <- cbind(Dx, Dy)
+    ref <- as.matrix(cbind(X, Y))
+    dimnames(ref) <- NULL
+    checkIdentical(as.matrix(Dr), ref) 
+
+    # Works with a mix of matrix types.
+    Dy2 <- DelayedArray(as.matrix(Y))
+    Dr2 <- cbind(Dx, Dy2)
+    checkIdentical(as.matrix(Dr2), ref) 
+
+    # Works with >2 matrices.
+    Dr3 <- cbind(Dx, Dy, Dx, Dy)
+    ref <- as.matrix(cbind(X, Y, X, Y))
+    dimnames(ref) <- NULL
+    checkIdentical(as.matrix(Dr3), ref) 
+}
+
+test_native_rbind <- function() {
+    X <- Matrix::rsparsematrix(1000, 100, 0.1)
+    Dx <- DelayedArray(X)
+    Y <- Matrix::rsparsematrix(5000, 100, 0.1)
+    Dy <- DelayedArray(Y)
+
+    Dr <- rbind(Dx, Dy)
+    ref <- as.matrix(rbind(X, Y))
+    dimnames(ref) <- NULL
+    checkIdentical(as.matrix(Dr), ref) 
+
+    # Works with a mix of matrix types.
+    Dy2 <- DelayedArray(as.matrix(Y))
+    Dr2 <- rbind(Dx, Dy2)
+    checkIdentical(as.matrix(Dr2), ref) 
+
+    # Works with >2 matrices.
+    Dr3 <- rbind(Dx, Dy, Dx, Dy)
+    ref <- as.matrix(rbind(X, Y, X, Y))
+    dimnames(ref) <- NULL
+    checkIdentical(as.matrix(Dr3), ref) 
+}
+
+test_native_trans <- function() {
+    X <- Matrix::rsparsematrix(1000, 100, 0.1)
+    Dx <- DelayedArray(X)
+
+    ref <- as.matrix(t(X))
+    dimnames(ref) <- NULL
+    checkIdentical(as.matrix(t(Dx)), ref) 
+
+    checkIdentical(as.matrix(t(t(Dx))), as.matrix(X))
+}
+
+test_native_ops <- function() {
+    X <- Matrix::rsparsematrix(1000, 100, 0.1)
+    Dx <- DelayedArray(X)
+
+    checkIdentical(as.matrix(X+1), as.matrix(Dx+1))
+    checkIdentical(as.matrix(X*2), as.matrix(Dx*2))
+
+    ref <- as.matrix(X > 0)
+    dimnames(ref) <- list(NULL, NULL)
+    checkIdentical(ref, as.matrix(Dx > 0))
+
+    Y <- Matrix::rsparsematrix(1000, 100, 0.1)
+    Dy <- DelayedArray(Y)
+
+    checkIdentical(as.matrix(X + Y), as.matrix(Dx + Dy))
+    checkIdentical(as.matrix(X * Y), as.matrix(Dx * Dy))
+}
+
+test_native_mult <- function() {
+    X <- Matrix::rsparsematrix(1000, 100, 0.1)
+    Dx <- DelayedArray(X)
+
+    mat <- matrix(runif(500), ncol=5)
+    checkIdentical(as.matrix(X %*% mat), as.matrix(Dx %*% mat))
+
+    mat2 <- matrix(runif(5000), nrow=5)
+    checkIdentical(as.matrix(mat2 %*% X), as.matrix(mat2 %*% Dx))
+
+    checkIdentical(as.matrix(crossprod(X, t(mat2))), as.matrix(crossprod(Dx, t(mat2))))
+    checkIdentical(as.matrix(crossprod(t(mat2), X)), as.matrix(crossprod(t(mat2), Dx)))
+
+    checkIdentical(as.matrix(tcrossprod(X, t(mat))), as.matrix(tcrossprod(Dx, t(mat))))
+    checkIdentical(as.matrix(tcrossprod(t(mat), X)), as.matrix(tcrossprod(t(mat), Dx)))
+
+    checkIdentical(as.matrix(crossprod(X)), as.matrix(crossprod(Dx)))
+    checkIdentical(as.matrix(tcrossprod(X)), as.matrix(tcrossprod(Dx)))
+}
